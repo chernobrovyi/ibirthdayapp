a3e14b7105139808b707d43fac2e97dacc603c9b
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2019-07-29 19:54:40 +0300

    Init commit

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Valeriy Chernobrovyi on 28/07/2019.
+//  Copyright © 2019 Valeriy Chernobrovyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
